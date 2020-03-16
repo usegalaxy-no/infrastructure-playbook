d8ba337b506bd2d8db21a5f5d3e7f456844634e4
brugger <kim.brugger@uib.no>
2020-03-16 15:40:18 +0100

    script can now read and write to a queue for connectivity testing

--- bin/rabbitmq_test_connection.py
+++ bin/rabbitmq_test_connection.py
@@ -13,14 +13,52 @@ import pika
 
 connection = None
 channel    = None
+exchange   = None
+channels   = []
 
 
-def connect(uri:str, ):
+def connect(uri:str, exchange:str='default', exchange_type:str='direct'):
   global connection
   global channel
-
+  global exchange
+  
   connection = pika.BlockingConnection( pika.connection.URLParameters(uri) )
   channel    = connection.channel()
+  exchange   = exchange
+  
+  channel.exchange_declare(exchange=exchange, exchange_type=exchange_type, durable=True)
+
+
+
+def _check_channel(name:str):
+  if name not in channels:
+    
+    result = channel.queue_declare(queue=name, durable=True)
+    
+    channel.queue_bind(exchange=exchange,
+                       queue=name,
+                       routing_key=name)
+    global channels
+    channels.append( name )
+            
+def publish(body:str, route:str='default'):
+
+  _check_channel( route)
+  channel.basic_publish( exchange=exchange, routing_key=route, body=body, properties=pika.BasicProperties( delivery_mode=2))
+        
+def consume(route:str='default', callback=None):
+  try:
+    _check_channel( route)
+    channel.basic_consume(queue=route, on_message_callback=callback)
+    channel.start_consuming()
+  except Exception as e:
+    print( e )
+
+
+def queue_length(queue:str=None):
+  result = channel.queue_declare(queue=queue, durable=True, passive=True)
+  return result.method.message_count
+    
 
 
 def main():
@@ -33,6 +71,6 @@ def main():
   url = args.url
 
   connect( url )
-
+  publish(body="Test publish to the queue")
 
 main()
