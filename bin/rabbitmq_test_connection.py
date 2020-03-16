ef08bb47afa642b39665f7a06892a3ba2823ec0c
brugger <kim.brugger@uib.no>
2020-03-16 15:41:40 +0100

    script can now read and write to a queue for connectivity testing

--- bin/rabbitmq_test_connection.py
+++ bin/rabbitmq_test_connection.py
@@ -13,64 +13,58 @@ import pika
 
 connection = None
 channel    = None
-exchange   = None
+exchange   = ''
 channels   = []
 
 
-def connect(uri:str, exchange:str='default', exchange_type:str='direct'):
+def connect(uri:str, exchange_type:str='direct'):
   global connection
   global channel
-  global exchange
-  
+
   connection = pika.BlockingConnection( pika.connection.URLParameters(uri) )
   channel    = connection.channel()
-  exchange   = exchange
-  
-  channel.exchange_declare(exchange=exchange, exchange_type=exchange_type, durable=True)
 
 
-
-def _check_channel(name:str):
-  if name not in channels:
-    
-    result = channel.queue_declare(queue=name, durable=True)
-    
-    channel.queue_bind(exchange=exchange,
-                       queue=name,
-                       routing_key=name)
-    global channels
-    channels.append( name )
-            
 def publish(body:str, route:str='default'):
 
-  _check_channel( route)
   channel.basic_publish( exchange=exchange, routing_key=route, body=body, properties=pika.BasicProperties( delivery_mode=2))
-        
+  print("Written {}".format( body ))
+
+def callback(ch, method, properties, body):
+  print(" [x] Received {}".format(body))
+  ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
 def consume(route:str='default', callback=None):
   try:
-    _check_channel( route)
+    channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue=route, on_message_callback=callback)
     channel.start_consuming()
   except Exception as e:
     print( e )
 
 
-def queue_length(queue:str=None):
-  result = channel.queue_declare(queue=queue, durable=True, passive=True)
-  return result.method.message_count
-    
-
 
 def main():
 
   parser = argparse.ArgumentParser(description='get-workflows: export workflows from a galaxy instance ')
   parser.add_argument('-u', '--url', required=True, help="uri to connect to, eg: amqp://user:password@host:port/vhost")
+  parser.add_argument('-w', '--write', action="store_true", help="write to vhost")
+  parser.add_argument('-r', '--read', action="store_true", help="read from vhost")
 
 
   args = parser.parse_args()
   url = args.url
 
   connect( url )
-  publish(body="Test publish to the queue")
+  if args.write:
+    publish(body="Test publish to the queue")
 
-main()
+  if args.read:
+    consume(callback=callback)
+
+
+if __name__ == '__main__':
+
+  main()
+
