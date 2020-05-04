cffb8f392b722f518195fa2f9a8249bec3557a54
brugger <kim.brugger@uib.no>
2020-05-04 18:42:15 +0200

    fixed the script so it now works with logrotate

--- bin/log_strip_out_uwsgi.py
+++ bin/log_strip_out_uwsgi.py
@@ -5,27 +5,23 @@ import sys
 import pprint as pp
 import gzip
 import re
+import os
 
-def open_outstream( outfile:str=None, compress:bool=False) -> int:
+def open_outstream( outfile:str=None) -> int:
     fh = sys.stdout
 
     if outfile is not None:
-        if compress:
-            fh = gzip.open(outfile, 'wt')
-        else:
-            fh = open(outfile, 'w')
+        fh = open(outfile, 'w')
 
     return fh
 
 
 
-def process_logfile(infile:str, mask:bool=False, outfile:str=None, compress:bool=False) -> None:
+def process_logfile(infile:str, mask:bool=False, outfile:str=None) -> None:
 
-    out_fh = open_outstream(outfile, compress)
-    print(infile)
 
     in_fh = open(infile, 'r')
-
+    out_fh = open_outstream(outfile)
 
     for line in in_fh:
         if re.search(r'uwsgi: \d+\.\d+\.\d+\.\d+', line) or re.search(r'uwsgi: \[pid:.* \d+\.\d+\.\d+\.\d+ ', line):
@@ -35,25 +31,29 @@ def process_logfile(infile:str, mask:bool=False, outfile:str=None, compress:bool
         else:
             out_fh.write( line )
 
-
-
-
-
 def main():
     parser = argparse.ArgumentParser(description='ehosd: the ehos daemon to be run on the master node ')
 
     parser.add_argument('-o', '--outfile', default=None, help="file to write to, default stdout")
-    parser.add_argument('-c', '--compress', default=False, action='store_true',  help="compress output when writing to an outfile")
     parser.add_argument('-m', '--mask', default=False, action='store_true',  help="mask rather than strip IP")
+    parser.add_argument('-r', '--replace', default=False, action='store_true',  help="replace original file with altered one")
     parser.add_argument('infile', metavar='infiles', nargs=1, help="infile to mask uwsgi IP's in")
-
-
 
     args = parser.parse_args()
     args.infile = args.infile[0]
-    pp.pprint( args )
 
-    process_logfile( args.infile, args.mask, args.outfile, args.compress)
+
+    if args.replace:
+        tempfile = "{}.tmp".format(args.infile)
+        os.rename(args.infile, tempfile)
+        args.infile, args.outfile = tempfile, args.infile
+
+
+    process_logfile( infile=args.infile, mask=args.mask, outfile=args.outfile)
+
+
+    if args.replace:
+        os.remove( tempfile )
 
 
 if __name__ == "__main__":
