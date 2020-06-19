2cc80219e5c2a2c26ede7bcb630325b70ec45d9a
brugger <kim.brugger@uib.no>
2020-06-19 10:54:52 +0200

    oidc spec files

---
+++ env/common/templates/galaxy/auth/nels.py
@@ -0,0 +1,30 @@
+"""
+Backend for OpenID Connect ELIXIR AAI
+https://www.elixir-europe.org/services/compute/aai
+"""
+
+from social_core.backends.open_id_connect import OpenIdConnectAuth
+
+
+class NeLSOpenIdConnect(OpenIdConnectAuth):
+    name = 'nels'
+    OIDC_ENDPOINT = 'https://test-fe.cbu.uib.no/oidc/'
+    EXTRA_DATA = [
+        ('expires_in', 'expires_in', True),
+        ('refresh_token', 'refresh_token', True),
+        ('id_token', 'id_token', True),
+        ('other_tokens', 'other_tokens', True),
+    ]
+
+    DEFAULT_SCOPE = ['openid', 'email']
+    JWT_DECODE_OPTIONS = {'verify_at_hash': False}
+
+    def get_user_details(self, response):
+        username_key = self.setting('USERNAME_KEY', default=self.USERNAME_KEY)
+        name = response.get('name') or ''
+        fullname, first_name, last_name = self.get_user_names(name)
+        return {'username': response.get(username_key),
+                'email': response.get('email'),
+                'fullname': fullname,
+                'first_name': first_name,
+                'last_name': last_name}
